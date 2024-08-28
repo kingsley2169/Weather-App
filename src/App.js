9b9067604bc20135eb388753da84a31c8eedda0f
Ikenna Kingsley Eze <164315@std.eul.edu.tr>
2024-08-29 02:24:02 +0300

    An app that shows the weather in any city in the world

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
+import Weather from './Weather';
 import './App.css';
 
+function Header({ city, setCity, fetchData }) {
+  const handleInputChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchData();
+  };
+
+  return (
+    <header className="header">
+      <h1>Weather App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search city"
+          value={city}
+          onChange={handleInputChange}
+          className="search-bar"
+        />
+        <button type="submit" className="search-button">Get Weather</button>
+      </form>
+    </header>
+  );
+}
+
 function App() {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
+      );
+      setWeatherData(response.data);
+      console.log(response.data); 
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header city={city} setCity={setCity} fetchData={fetchData} />
+      <Weather weatherData={weatherData} />
     </div>
   );
 }
